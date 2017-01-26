1459909b6b9cec6d05856c94a675fd123f5efecd
francisco <contact@pacogomez.com>
2017-01-25 19:03:32 -0600

    wip

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 
     source ~/prj/ansible/hacking/env-setup
     source ./env-setup
+
+
+https://download3.vmware.com/software/vmw-tools/nested-esxi/Nested_ESXi6.x_Appliance_Template_v5.ova
+https://download3.vmware.com/software/vmw-tools/nested-esxi/Nested_ESXi6.5_Appliance_Template_v1.ova
+
+~/prj/ansible/hacking/test-module -m ./ovftool.py -a "ovftool_path=\"/Applications/VMware OVF Tool\" vcenter=10.158.13.196 vcenter_user=administrator@vsphere.local vcenter_password=${PASSWORD} vm_name=esx1 datacenter=dctr1 datastore=SAP-3TB-2 portgroup='VM Network' cluster=cluster1 path_to_ova=. ova_file=Nested_ESXi6.x_Appliance_Template_v5.ova"
+
+
+Install the module:
+
+    wget https://raw.githubusercontent.com/pacogomez/ansible-module-ovftool/master/ovftool.py
--- ovftool.py
+++ ovftool.py
@@ -42,7 +42,13 @@ def main():
             vm_name=dict(required=True, type='str'),
             vcenter=dict(required=True, type='str'),
             vcenter_user=dict(required=True, type='str'),
-            vcenter_password=dict(required=True, type='str', no_log=True)
+            vcenter_password=dict(required=True, type='str', no_log=True),
+            datacenter=dict(required=True, type='str'),
+            cluster=dict(required=True, type='str'),
+            datastore=dict(required=True, type='str'),
+            portgroup=dict(required=True, type='str'),
+            path_to_ova=dict(required=True, type='str'),
+            ova_file=dict(required=True, type='str'),
         ),
         supports_check_mode=True,
     )
@@ -59,7 +65,42 @@ def main():
     if target_vm:
         module.fail_json(msg='A VM with the name {} is already present'.format(module.params['vm_name']))
 
-    module.exit_json(changed=False)
+    ovftool_exec = '{}/ovftool'.format(module.params['ovftool_path'])
+    ova_file = '{}/{}'.format(module.params['path_to_ova'], module.params['ova_file'])
+    vi_string = 'vi://{}:{}@{}/{}/host/{}/'.format(module.params['vcenter_user'],
+                                                   module.params['vcenter_password'], module.params['vcenter'],
+                                                   module.params['datacenter'], module.params['cluster'])
+
+    ova_tool_result = module.run_command([ovftool_exec,
+                                          '--acceptAllEulas',
+                                          '--skipManifestCheck',
+                                          '--powerOn',
+                                          '--noSSLVerify',
+                                          '--allowExtraConfig',
+                                          # '--diskMode={}'.format(module.params['disk_mode']),
+                                          '--datastore={}'.format(module.params['datastore']),
+                                          # TODO: specify network to map
+                                          '--network={}'.format(module.params['portgroup']),
+                                          '--name={}'.format(module.params['vm_name']),
+                                          # '--prop:vsm_hostname={}'.format(module.params['hostname']),
+                                          # '--prop:vsm_dns1_0={}'.format(module.params['dns_server']),
+                                          # '--prop:vsm_domain_0={}'.format(module.params['dns_domain']),
+                                          # '--prop:vsm_ntp_0={}'.format(module.params['ntp_server']),
+                                          # '--prop:vsm_gateway_0={}'.format(module.params['gateway']),
+                                          # '--prop:vsm_ip_0={}'.format(module.params['ip_address']),
+                                          # '--prop:vsm_netmask_0={}'.format(module.params['netmask']),
+                                          # '--prop:vsm_cli_passwd_0={}'.format(module.params['admin_password']),
+                                          # '--prop:vsm_cli_en_passwd_0={}'.format(module.params['enable_password']),
+                                          ova_file, 
+                                          vi_string])
+
+    if ova_tool_result[0] != 0:
+        module.fail_json(msg='Failed to deploy OVA, error message from ovftool is: {}'.format(ova_tool_result[1]))
+    # if not wait_for_api(module):
+    #     module.fail_json(msg='Failed to deploy OVA, timed out waiting for the API to become available')
+
+    module.exit_json(changed=True, ova_tool_result=ova_tool_result)
+
 
 from ansible.module_utils.basic import *
 
